910c9652df2dd516d1d6b665170776e8a14b1b28
Emerson Pender <pen0002@jmss.it>
2012-08-14 12:52:03 +1000

    Function moving and fixed unknown crash error: (CODE) dev 1.4

--- Main/Functions.py
+++ Main/Functions.py
@@ -5,6 +5,9 @@ Created on 28/06/2012
 '''
 import string
 import datetime
+import os
+import urllib
+import time
 
 def listGet(lineNumber, lang): #lineNumber starts at 0. So line 1 is 0, 2 1 and so on
     filename = lang+"lib.csv"
@@ -35,4 +38,71 @@ def logMakeEN(lfd, verP, verN):
 def logWrite(logname, value):
     filename = logname+'txt'
     logOpen = open(filename, 'w')
-    
+
+def verUpd(info):
+    try:
+        verNf = open(info["verIN"],'r')
+        verN = verNf.readlines()
+        verNf.close()
+        info["verL"] = verN[1] 
+        need = True
+    except IOError:
+        os.remove('VersionIN.txt')
+        urllib.urlretrieve('http://ultrazoidserver.x10.mx/klapperen/version.txt',info["verIN"])
+        verNf = open(info["verIN"],'r')
+        verN = verNf.readlines()
+        verNf.close()
+        info["verL"] = verN[1] 
+        need = False
+    if need == True:
+        urllib.urlretrieve('http://ultrazoidserver.x10.mx/klapperen/version.txt','VersionIN.txt')
+        verINf = open('VersionIN.txt','r')
+        verIN = verINf.readlines()
+        verINf.close()
+        info["verLN"] = verIN[1]
+
+def noLog(lang, langL):
+    print 'NO LIBRARY FOUND'
+    dldInp = raw_input('WOULD YOU LIKE TO DOWNLOAD THE '+langL+' LIBRABRY(y/n)')
+    cont = False
+    while cont != True:
+        if dldInp == 'y':
+            print "Downloading"+lang+"lib.csv"
+            urllib.urlretrieve('http://ultrazoidserver.x10.mx/klapperen/library/'+lang+'lib.csv',lang+'lib.csv')
+            time.sleep(2)
+            print 'Downloaded'                
+            cont = True
+        elif dldInp == 'n':
+            print 'Program will now exit'
+            print 'Please obtain a copy of the library'
+            os.system('PAUSE')
+            os.sys.exit()
+        elif dldInp != 'y' or dldInp != 'n':
+            print 'PLEASE ENTER A VALID RESPONSE.'
+            dldInp = raw_input('WOULD YOU LIKE TO DOWNLOAD THE '+langL+ 'ONE(y/n)')
+
+def logUpd(lang, info):
+    print "Klapperen will now check to make sure "+lang+"lib.csv is up to date."
+    if info["verLN"] != info["verL"]:
+        print "There is an update for "+lang+"lib.csv."
+        libUp = raw_input("update?:(y/n)")
+        cont = False
+        while cont != True:
+            if libUp == 'y':
+                os.remove(lang+'lib.csv')
+                urllib.urlretrieve('http://ultrazoidserver.x10.mx/klapperen/library/langlib.csv',lang+'lib.csv')
+                os.remove('Version.txt')
+                urllib.urlretrieve('http://ultrazoidserver.x10.mx/klapperen/version.txt',info["verIN"])
+                print "UPDATE FINISHED"
+                raw_input("Press enter to continue...")
+                cont = True
+            elif libUp == 'n':
+                print 'Update aborted'
+                print 'Program will now continue'
+                raw_input("Press enter to continue...")
+                cont = True
+            elif libUp != 'y' or libUp != 'n':
+                print "Please enter a valid response:"
+    elif info["verLN"] == info["verL"]:
+        print "No update needed langlib.csv is up to date"
+        raw_input("Press enter to continue...")
--- Main/Main.py
+++ Main/Main.py
@@ -4,7 +4,7 @@ Created on 28/06/2012
 @author: ultrazoid_
 
 Klapperen Chatbot
-Version (CODE) dev 1.3
+Version (CODE) dev 1.4
 '''
 import string
 import time
@@ -14,30 +14,12 @@ import datetime
 import urllib
 
 verP = '(CODE) dev'
-verNu = '1.3'
+verNu = '1.4'
 verPN = verP+' '+verNu
 info = {"Name":"Klapperen","Version":verPN,"verIN":"Version.txt","verL":"1","verLN":"1"}
 
-try:
-    verNf = open(info["verIN"],'r')
-    verN = verNf.readlines()
-    verNf.close()
-    info["verL"] = verN[1] 
-    need = True
-except IOError:
-    os.remove('VersionIN.txt')
-    urllib.urlretrieve('http://ultrazoidserver.x10.mx/klapperen/version.txt',info["verIN"])
-    verNf = open(info["verIN"],'r')
-    verN = verNf.readlines()
-    verNf.close()
-    info["verL"] = verN[1] 
-    need = False
-if need == True:
-    urllib.urlretrieve('http://ultrazoidserver.x10.mx/klapperen/version.txt','VersionIN.txt')
-    verINf = open('VersionIN.txt','r')
-    verIN = verINf.readlines()
-    verINf.close()
-    info["verLN"] = verIN[1]
+
+Functions.verUpd(info)
 
 now = datetime.datetime.now()
 lfd = now.strftime("%Y-%d-%m %H%M")
@@ -51,58 +33,17 @@ if LanInp == 'EN':
     print 'ENGLISH SELECTED'
     Functions.logMakeEN(lfd,verP,verNu)
     try:
-        print "Klapperen will now check to make sure ENlib.csv is up to date."
-        if info["verLN"] != info["verL"]:
-            print "There is an update for ENlib.csv."
-            libUp = raw_input("update?:(y/n)")
-            cont = False
-            while cont != True:
-                if libUp == 'y':
-                    os.remove('ENlib.csv')
-                    urllib.urlretrieve('http://ultrazoidserver.x10.mx/klapperen/library/ENlib.csv','ENlib.csv')
-                    os.remove('Version.txt')
-                    urllib.urlretrieve('http://ultrazoidserver.x10.mx/klapperen/version.txt',info["verIN"])
-                    print "UPDATE FINISHED"
-                    os.system("PAUSE")
-                    cont = True
-                elif libUp == 'n':
-                    print 'Update aborted'
-                    print 'Program will now continue'
-                    os.system('PAUSE')
-                    cont = True
-                elif libUp != 'y' or libUp != 'n':
-                    print "Please enter a valid response:"
-        elif info["verLN"] == info["verL"]:
-            print "No update needed ENlib.csv is up to date"
-            os.system("PAUSE")
+        Functions.logUpd('EN',info)
         greetings = Functions.listGet(0,'EN')
         farewells = Functions.listGet(1,'EN')
         sGreetings = Functions.listGet(2,'EN')
         questions = Functions.listGet(3,'EN')
         stalls = Functions.listGet(4,'EN')
         print "SUCCESS"
-        os.system('PAUSE')
+        raw_input("Press enter to continue...")
         lib = True
     except IOError:
-        print 'NO LIBRARY FOUND'
-        dldInp = raw_input('WOULD YOU LIKE TO DOWNLOAD THE ENGLISH ONE(y/n)')
-        cont = False
-        lib = False
-        while cont != True:
-            if dldInp == 'y':
-                print "Downloading ENlib.csv"
-                urllib.urlretrieve('http://ultrazoidserver.x10.mx/klapperen/library/ENlib.csv','ENlib.csv')
-                time.sleep(2)
-                print 'Downloaded'                
-                cont = True
-            elif dldInp == 'n':
-                print 'Program will now exit'
-                print 'Please obtain a copy of the library'
-                os.system('PAUSE')
-                os.sys.exit()
-            elif dldInp != 'y' or dldInp != 'n':
-                print 'PLEASE ENTER A VALID RESPONSE.'
-                dldInp = raw_input('WOULD YOU LIKE TO DOWNLOAD THE ENGLISH ONE(y/n)')
+        Functions.noLog('EN', 'ENGLISH')
         try:
             greetings = Functions.listGet(0,'EN')
             farewells = Functions.listGet(1,'EN')
@@ -110,10 +51,10 @@ if LanInp == 'EN':
             questions = Functions.listGet(3,'EN')
             stalls = Functions.listGet(4,'EN')
             print "SUCCESS"
-            os.system('PAUSE')
+            raw_input("Press enter to continue...")
             lib = True
         except IOError:
             print 'FATAL ERROR ENCOUNTED'
             print 'PROGRAM WILL NOW SHUT DOWN'
-            os.system('PAUSE')
+            raw_input("Press enter to continue...")
                 
